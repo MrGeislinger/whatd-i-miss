ce351346919e47c86cc922426e2266b72e9d1981
MrGeislinger <9027783+MrGeislinger@users.noreply.github.com>
2023-06-04 09:57:31 -0700

    MODELS into assistant.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from data import (
     only_most_similar_embeddings,
     text_to_sentences,
 )
-from assistant import ask_claude, calculate_tokens
+from assistant import ask_claude, calculate_tokens, MODELS
 from prompt import create_prompt
 from postprocess import (
     extract_json,
@@ -27,80 +27,6 @@ logger.info('Start web app')
 SENTENCE_SEPARATOR = '\n'
 SECTION_SEPARATOR_START = '<section>\n'
 SECTION_SEPARATOR_END = '\n</section>'
-# From https://console.anthropic.com/docs/api/reference#parameters
-MODELS: dict[str,str] = {
-    "claude-instant-v1.1-100k": (
-        100_000,
-        "An enhanced version of claude-instant-v1.1 with a 100,000 token"
-        "context window that retains its lightning fast 40 word/sec "
-        "performance."
-    ),
-    "claude-instant-v1-100k": (
-        100_000,
-        "An enhanced version of claude-instant-v1 with a 100,000 token context "
-        "window that retains its performance. Well-suited for high throughput "
-        "use cases needing both speed and additional context, allowing deeper "
-        "understanding from extended conversations and documents."
-    ),
-    "claude-v1.3-100k": (
-        100_000,
-        "An enhanced version of claude-v1.3 with a 100,000 token (roughly "
-        "75,000 word) context window."
-    ),
-    "claude-instant-v1.1": (
-        8_000,
-        "Our latest version of claude-instant-v1. It is better than "
-        "claude-instant-v1.0 at a wide variety of tasks including writing, "
-        "coding, and instruction following. It performs better on academic "
-        "benchmarks, including math, reading comprehension, and coding tests. "
-        "It is also more robust against red-teaming inputs."
-    ),
-    "claude-instant-v1": (
-        8_000,
-        "A smaller model with far lower latency, sampling at roughly 40 "
-        "words/sec! Its output quality is somewhat lower than the latest "
-        "claude-v1 model, particularly for complex tasks. However, it is much "
-        "less expensive and blazing fast. We believe that this model provides "
-        "more than adequate performance on a range of tasks including text "
-        "classification, summarization, and lightweight chat applications, as "
-        "well as search result summarization."
-    ),
-    "claude-instant-v1.0": (
-        8_000,
-        "An earlier version of claude-instant-v1."
-    ),
-    "claude-v1-100k": (
-        8_000,
-        "An enhanced version of claude-v1 with a 100,000 token (roughly"
-        "75,000 word) context window. Ideal for summarizing, analyzing, and "
-        "querying long documents and conversations for nuanced understanding "
-        "of complex topics and relationships across very long spans of text."
-    ),
-    "claude-v1.3": (
-        8_000,
-        "Compared to claude-v1.2, it's more robust against red-team inputs"
-        " better at precise instruction-following, better at code, and better "
-        "and non-English dialogue and writing."
-    ),
-    "claude-v1.2": (
-        8_000,
-        "An improved version of claude-v1. It is slightly improved at general "
-        "helpfulness, instruction following, coding, and other tasks. It is "
-        "also considerably better with non-English languages. This model also "
-        "has the ability to role play (in harmless ways) more consistently, "
-        "and it defaults to writing somewhat longer and more thorough "
-        "responses."
-    ),
-    "claude-v1": (
-        8_000,
-        "Our largest model, ideal for a wide range of more complex tasks."
-    ),
-    "claude-v1.0": (
-        8_000,
-        "An earlier version of claude-v1."
-    ),
-}
-
 
 ##### HEADER
 
